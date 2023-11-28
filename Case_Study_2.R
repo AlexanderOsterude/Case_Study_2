bea498a2c41aad30d021d60ddd874080480d383d
AlexanderOsterude <116122131+AlexanderOsterude@users.noreply.github.com>
2023-11-27 22:48:36 -0500

    Add files via upload

--- Case_Study_2.R
+++ Case_Study_2.R
@@ -30,3 +30,17 @@ pie(labels = c("Graduated", "Dropped Out"), c(grad_morning,sum(subset(classTime,
 
 prop.test(c(grad_evening, grad_morning), c(nrow(subset(classTime, Time == '0')), nrow(subset(classTime, Time == '1'))), correct = FALSE, alternative = "less")
 
+
+## 8.1 ----
+genderData <- data.frame(Gender = data$Gender, Outcome = data$Target)
+maleGraduation <- sum(subset(genderData, Gender == '1')$Outcome == "Graduate")
+femaleGraduation <-sum(subset(genderData, Gender == '0')$Outcome == "Graduate")
+
+prop.test(c(maleGraduation, femaleGraduation), c(nrow(subset(genderData, Gender == '1')), nrow(subset(genderData, Gender == '0'))), correct = FALSE)
+
+## 8.2 ----
+scholarshipData <- data.frame(Scholarship.Status = data$Scholarship.holder, Outcome = data$Target)
+scholarshipGraduation <- sum(subset(scholarshipData, Scholarship.Status == '1')$Outcome == "Graduate")
+nonScholarshipGraduation <-sum(subset(scholarshipData, Scholarship.Status == '0')$Outcome == "Graduate")
+
+prop.test(c(scholarshipGraduation, nonScholarshipGraduation), c(nrow(subset(scholarshipData, Scholarship.Status == '1')), nrow(subset(scholarshipData, Scholarship.Status == '0'))), correct = FALSE)
